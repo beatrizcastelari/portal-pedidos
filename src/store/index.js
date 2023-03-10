8fd94fe9b40106d6cca9b9f248709d60527fb86f
beatrizcastelari <beatrizmess95@gmail.com>
2023-03-09 21:29:35 -0300

    changed pizza image

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ export default createStore({
     pizzaJson: [
       { id: 1, name: "Calabresa", img: "assets/pizzacalabresa.jpg", price: 20.19, description: "Calabresa,queijo", category: "promocao" },
       { id: 2, name: "Bauru", img: "assets/pizzabauru.jpg", price: 18.00, description: "Presunto,queijo,tomate", category: "promocao" },
-      { id: 3, name: "Marguerita", img: "assets/marguerita.jpg", price: 17.45, description: "Mussarela,molho de tomate,manjericão", category: "normal" },
+      { id: 3, name: "Marguerita", img: "assets/pizzabauru.jpg", price: 17.45, description: "Mussarela,molho de tomate,manjericão", category: "normal" },
       { id: 4, name: "Moda da Casa", img: "assets/moda.jpg", price: 19.77, description: "Presunto,queijo,tomate,bacon", category: "normal" },
       { id: 5, name: "Portuguesa", img: "assets/pizzaportuguesa.jpg", price: 21.43, description: "Presunto,queijo,ovo,milho", category: "normal" },
       { id: 6, name: "Chocolate", img: "assets/chocolate.jpg", price: 18.55, description: "Chocolate,morango", category: "doce" },
